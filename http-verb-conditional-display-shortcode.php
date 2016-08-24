09ae7faa5919f2d6f28c020e256dd669fd6483c1
Shawn Hooper <shawn@actionablebooks.com>
2016-08-24 01:38:59 -0400

    added shortcode for get only

--- http-verb-conditional-display-shortcode.php
+++ http-verb-conditional-display-shortcode.php
@@ -2,20 +2,21 @@
 /*
 Plugin Name: HTTP Verb Conditional Display Shortcode
 Description: Allows you to conditionally display a block of content depending on the HTTP verb (GET, POST, etc) used to call the page.
-Version: 1.0.0
+Version: 1.0.3
 Author: Shawn Hooper, Actionable
 */
 
 class HTTPVerbShortcodePlugin {
 
 	function __construct() {
-		add_shortcode('http_verb_post_only', array( $this, 'render_shortcode_post_only' ) );
+		add_shortcode( 'http_post_only', array( $this, 'render_shortcode_post_only' ) );
+		add_shortcode( 'http_get_only', array( $this, 'render_shortcode_get_only' ) );
 	}
 	
 	function render_shortcode_get_only( $atts, $content = null ) {
 		global $_post;
 
-		if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+		if ( $_SERVER['REQUEST_METHOD'] === 'GET' ) {
 		     return $content;
 		}
 		
@@ -25,7 +26,7 @@ class HTTPVerbShortcodePlugin {
 	function render_shortcode_post_only( $atts, $content = null ) {
 		global $_post;
 
-		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 		     return $content;
 		}
 		
